11b9b38fc3227a04809a2a98ea89b77558eade50
hiromu.tsuruta <hiromu.tsuruta@rakuten.com>
2021-03-06 15:57:10 +0900

    generate project

---
+++ CompositionalLayouts-MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayouts-MVP
+//
+//  Created by Tsuruta, Hiromu | ECID on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
